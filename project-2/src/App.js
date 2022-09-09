e58aa5b0266e775e1fa2cdab23ef3f7c6e741b91
CharlyWells <92895394+CharlyWells@users.noreply.github.com>
2022-09-09 00:08:53 -0700

    commit

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,67 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react'
 import LoginForm from './components/LoginForm';
-import "./App.css";
 
 function App() {
-  
-  const adminUser  = {
-    name: "Admin143",
-    email: "wellingtoncharlottenaaodarley@gmail.com",
-    password: "admin12341234"
-  }
-   
-  const doctor = {
-   
-    name: "newdoc",
-    email: "",
-    password: "doc12341234"
-  }
-
-  const nurse = {
-    name: "newnurse",
-    email: "",
-    password: "nurse12341234"
-  }
-
-
-
-  const Patient = {
-    username: "newuser",
-    email: "",
-    password: "patient12341234"
-  }
- 
-
-  const [user, setUser] = useState({name: "", email: ""});
-  const [error, setError]= useState("");
-
-  const Login = details => {
-      console.log(details);
-
-      if(details.email == adminUser.email && details.password==adminUser.password){
-      console.log("Logged in");
-  } else{
-    console.log("Details do not match!");
-  }
-
-  const Logout = () => {
-    console.log("Logout");
-  }
-
   return (
-    <div className ="App">
-        {(user.email !== "") ? (
-          <div className='welcome'>
-            <h2>Welcome, <span>{user.name}</span></h2>
-            <button>Logout</button>
-          </div>
-        ): (
-          <LoginForm Login={Login} error={error}/>
-        )}
+    <div style= {{backgroundColor:"#white " ,    }}>
+    <LoginForm />
     </div>
   )
 }
-}
 
-export default App;
-
+export default App
--- project-2/src/components/LoginForm.js
+++ project-2/src/components/LoginForm.js
@@ -1,34 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-function LoginForm({ Login, error}) {
-    const [details, setDetails] = useState({name: "", email: "", password:""});
-    const submitHandler = e => {
-        e.preventDefault();
-
-        Login(details);
-    }
+function LoginForm() {
     return (
-         <form onSubmit={submitHandler}>
-            <div className='form-inner'>
-                <h2>Login</h2>
-                {/* ERROR! */}
-                <div className='form-group'></div>
-                   <label htmlFor='name'>Name:</label>
-                   <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
-            </div>
-            <div className='form-group'>
-                <label htmlFor='email'>Email: </label>
-                <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
-            </div>
-            <div className='form-group'>
-                <label htmlFor='password'>Password: </label>
-                <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
-            </div>
-            <div>
-              <input type="submit" value="LOGIN"/>
-            </div>
-         </form>
+        <section className='section-1'>
+        <h2>Sign-In</h2>
+        <p>Access the DPMG1 panel using your username and passcode</p>
+        </section>
+
+
+        //Add your form below:
+     
     )
 }
 
-export default LoginForm;
+export default LoginForm
