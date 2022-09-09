8fbad69133900ceb617e0416993d5163374d9aef
CharlyWells <charlottenaaodarleywellington@gmail.com>
2022-09-08 18:04:37 -0700

    commit

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,11 +1,53 @@
-import React from 'react';
-import Form from "./components/Form";
+import React, {useState} from 'react';
+import LoginForm from './components/LoginForm';
 import "./App.css";
 
 function App() {
+  /*
+  const adminUser  = {
+    email: "wellingtoncharlottenaaodarley@gmail.com",
+    password: "admin12341234"
+  }
+   
+  const doctor = {
+    username: "newdoc",
+    password: "doc12341234"
+  }
+
+  const nurse = {
+    username: "newnurse",
+    password: "nurse12341234"
+  }
+
+
+
+  const Patient = {
+    username: "newuser",
+    password: "patient12341234"
+  }
+  */
+
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError]= useState("");
+
+  const Login = details => {
+      console.log(details);
+  }
+
+  const Logout = () => {
+    console.log("Logout");
+  }
+
   return (
-    <div>
-      <form />
+    <div className ="App">
+        {(user.email !== "") ? (
+          <div className='welcome'>
+            <h2>Welcome, <span>{user.name}</span></h2>
+            <button>Logout</button>
+          </div>
+        ): (
+          <LoginForm />
+        )}
     </div>
   )
 }
