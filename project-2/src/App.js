89a6b2ff74592cfff257611f819b5307dcb99e9c
CharlyWells <92895394+CharlyWells@users.noreply.github.com>
2022-09-11 04:53:17 -0700

    commit

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import LoginForm from './components/LoginForm';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div style= {{backgroundColor:"#white"}}>
     <LoginForm />
     </div>
+    </Router>
   )
 }
 
