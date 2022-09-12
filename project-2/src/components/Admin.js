088a6144039ac4e4ed95f526022289acff6289dc
CharlyWells <92895394+CharlyWells@users.noreply.github.com>
2022-09-12 15:45:06 -0700

    cmmit

--- project-2/src/components/Admin.js
+++ project-2/src/components/Admin.js
@@ -12,17 +12,15 @@ function Admin() {
       
     
       <section></section>
-      <div class='row'>
+      
         <div class='day square'><h3>Total Clients</h3></div>
-        <div class='task square'></div>
         
-      </div>
-      <div class='row secondary-background'>
+        
+   
+      
         <div class='day 1'><h3>New Registration</h3></div>
-        <div class='task square'></div>
-        <div class='task square'></div>
-        <div class='task square'></div>
-      </div>
+       
+      
       <div class='row'>
         <div class='day 2'><h3>Appointments History</h3></div>
         <div class='task square'></div>
