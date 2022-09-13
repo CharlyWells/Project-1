247376e4bcd287d24032d9b7e34160e01175ad06
CharlyWells <92895394+CharlyWells@users.noreply.github.com>
2022-09-12 21:24:00 -0700

    commit

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import {useState} from 'react'
 import LoginForm from './components/LoginForm';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Admin from './components/Admin';
+import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
+
+     const[sidebarOpen, setSidebarOpen] = useState(false);
+
+     const openSidebar = () => {
+      setSidebarOpen(true);
+     }
+
+     const closeSidebar = () => {
+        setSidebarOpen(false);
+     }
+
   return (
     <Router>
     <div style= {{backgroundColor:"#white"}}>
+
     <LoginForm />
     <Admin/>
+    <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar}/>
+    
+    
+       
+    <div class="container">
+        <h1>React Dashboard</h1>
+    </div>
+    
+      
     </div>
     </Router>
   )
--- project-2/src/components/Admin.js
+++ project-2/src/components/Admin.js
@@ -6,40 +6,7 @@ import Logo  from "../images/Logo.png"
 
 function Admin() {
     return (
-        <div id='header'>
-        <img src={Logo}  alt=""  className="img-2"/>
-        <figcaption class='group-1'> DPMG1</figcaption>
-      
-    
-      <section></section>
-      
-        <div class='day square'><h3>Total Clients</h3></div>
-        
-        
-   
-      
-        <div class='day 1'><h3>New Registration</h3></div>
-       
-      
-      <div class='row'>
-        <div class='day 2'><h3>Appointments History</h3></div>
-        <div class='task square'></div>
-        <div class='task square'></div>
-        <div class='task square'></div>
-      </div>
-      <div class='row secondary-background'>
-        <div class='day 3'><h3>Physician Visit</h3></div>
-        <div class='task square'></div>
-        <div class='task square'></div>
-        <div class='task square'></div>
-      </div>
-      </div>
-    
-
-
-
-
-  
+      <div></div>
 
      
     )
